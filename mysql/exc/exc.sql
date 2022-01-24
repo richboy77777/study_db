fc99c295666563f02efa1ccdf9a6a1d72c5c1aca
richboy77777 <goodw@DESKTOP-AFQRPVH>
2022-01-24 11:58:21 +0900

    1월 24일 월요일

---
+++ mysql/exc/exc.sql
@@ -0,0 +1,18 @@
+use world;
+
+show tables;
+
+
+create table member (
+    seq int,
+    name varchar(100),
+    id varchar(100)
+    regdate datetime
+);
+
+create table member (
+    seq int,
+    name varchar(100),
+    id varchar(100)
+    regdate datetime
+);
